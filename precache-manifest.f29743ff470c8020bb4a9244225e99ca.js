a787df0e71fbf1a3853e6a0d942ccce8d017b84d
casey0808 <casey_c0808@outlook.com>
2020-01-28 13:03:11 +0800

    Updates

---
+++ precache-manifest.f29743ff470c8020bb4a9244225e99ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca89d0868c2f50f74b69fc955f5f154c",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "863dae4042727a5a8850",
+    "url": "/my-app/static/css/main.a42a2ec8.chunk.css"
+  },
+  {
+    "revision": "c12da648722e46ab7bb3",
+    "url": "/my-app/static/js/2.c506d3cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.c506d3cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "863dae4042727a5a8850",
+    "url": "/my-app/static/js/main.a18beea8.chunk.js"
+  },
+  {
+    "revision": "6c5739c0324eebde34d0",
+    "url": "/my-app/static/js/runtime-main.0fc2de67.js"
+  }
+]);
